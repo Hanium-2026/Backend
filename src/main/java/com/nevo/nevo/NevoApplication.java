3060dcea74a2b83108db68b620c8b1212c3a84af
Chungs0604 <alsk3528@gmail.com>
2026-05-10 01:25:00 +0900

    🎉 Start: first commit

---
+++ src/main/java/com/nevo/nevo/NevoApplication.java
@@ -0,0 +1,13 @@
+package com.nevo.nevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NevoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nevo/nevo/NevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nevo.nevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
