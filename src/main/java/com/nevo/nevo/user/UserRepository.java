4d98edd65d13fd26e4d9044495b01175475b3937
hyunjun <y.raymond0211@gmail.com>
2026-05-11 13:03:04 +0900

    fix: 이메일 중복 확인 Repository 메서드명 수정

--- src/main/java/com/nevo/nevo/user/UserRepository.java
+++ src/main/java/com/nevo/nevo/user/UserRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 이메일로 사용자 조
+    // 이메일로 사용자 조회
     Optional<User> findByEmail(String email);
 
     // 이메일 중복 가입 여부 확인
-    boolean alreadyExistEmail(String email);
+    boolean existsByEmail(String email);
 }
