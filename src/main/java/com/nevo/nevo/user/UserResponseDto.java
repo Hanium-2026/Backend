45dd07d31d77c33a3e2e2d145da9672d313cfbee
hyunjun <y.raymond0211@gmail.com>
2026-05-11 12:30:15 +0900

    메서드 이름 오타 수정 form -> from

--- src/main/java/com/nevo/nevo/user/UserResponseDto.java
+++ src/main/java/com/nevo/nevo/user/UserResponseDto.java
@@ -1,7 +1,7 @@
 package com.nevo.nevo.user;
 
 public record UserResponseDto(Long id, String email, String name, Role role) {
-    public static UserResponseDto form(User user) {
+    public static UserResponseDto from(User user) {
         return new UserResponseDto(
                 user.getId(),
                 user.getEmail(),
