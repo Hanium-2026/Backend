2204e497b6604915940a7d783743573ebd1e5231
hyunjun <y.raymond0211@gmail.com>
2026-05-13 19:51:00 +0900

    메서드 수정

--- src/main/java/com/nevo/nevo/user/repository/UserRepository.java
+++ src/main/java/com/nevo/nevo/user/repository/UserRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 이메일로 사용자 조회
-    Optional<User> findByEmail(String email);
+    // 이메일로 활성 사용자 조회
+    Optional<User> findByEmailAndDeletedAtIsNull(String email);
 
-    // 이메일 중복 가입 여부 확인
-    boolean existsByEmail(String email);
+    // 이메일 중복 가입 여부 확인, 탈퇴하지 않은 사용자 기준
+    boolean existsByEmailAndDeletedAtIsNull(String email);
+
+    // ID로 활성 사용자 조회
+    Optional<User> findByIdAndDeletedAtIsNull(Long id);
 }
