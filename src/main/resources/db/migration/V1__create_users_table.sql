f8f1033888fc29fb276810f3d658d0deb731c12e
hyunjun <y.raymond0211@gmail.com>
2026-05-13 16:34:15 +0900

    created_at & updated_at 추가

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,11 +1,13 @@
 -- V1__create_users_table.sql
 
 CREATE TABLE users (
-    user_id BIGSERIAL PRIMARY KEY, -- PostgreSQL에서 자동 증가를 위한 BIGSERIAL
+    user_id BIGSERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL,
     fcm_token VARCHAR(255),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
