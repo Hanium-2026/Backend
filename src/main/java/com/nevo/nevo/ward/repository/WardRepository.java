6b55811d8f044577cbc256d51a188369d4369e10
hyunjun <y.raymond0211@gmail.com>
2026-05-14 01:03:10 +0900

    Ward Repository 코드 추가

---
+++ src/main/java/com/nevo/nevo/ward/repository/WardRepository.java
@@ -0,0 +1,7 @@
+package com.nevo.nevo.ward.repository;
+
+import com.nevo.nevo.ward.entity.Ward;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WardRepository extends JpaRepository<Ward, Long> {
+}
