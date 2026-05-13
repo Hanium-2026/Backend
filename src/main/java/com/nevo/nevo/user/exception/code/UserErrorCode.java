2e1aa20729d872534114bfbe10d62c04f7f0432d
hyunjun <y.raymond0211@gmail.com>
2026-05-13 19:49:45 +0900

    USER_NOT_FOUND 추가

--- src/main/java/com/nevo/nevo/user/exception/code/UserErrorCode.java
+++ src/main/java/com/nevo/nevo/user/exception/code/UserErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-    WARD_NOT_FOUND(HttpStatus.NOT_FOUND, "USER404", "피보호자 정보를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER404", "사용자 정보를 찾을 수 없습니다."),
+    WARD_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_WARD404", "피보호자 정보를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
