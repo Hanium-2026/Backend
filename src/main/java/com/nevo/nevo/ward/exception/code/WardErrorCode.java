f5b1c03543248deeb58a5d85a1f29b3438636505
hyunjun <y.raymond0211@gmail.com>
2026-05-14 17:10:35 +0900

    ✨ Feat: 로그인 api 추가

--- src/main/java/com/nevo/nevo/ward/exception/code/WardErrorCode.java
+++ src/main/java/com/nevo/nevo/ward/exception/code/WardErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum WardErrorCode implements ErrorCode {
 
-    WARD_NOT_FOUND(HttpStatus.NOT_FOUND, "WARD4040", "노약자 정보를 찾을 수 없습니다.");
+    WARD_NOT_FOUND(HttpStatus.NOT_FOUND, "WARD4040", "노약자 정보를 찾을 수 없습니다."),
+    WARD_ACCESS_DENIED(HttpStatus.FORBIDDEN, "WARD403", "노약자만 접근 가능한 기능입니다.");
 
     private final HttpStatus status;
     private final String code;
